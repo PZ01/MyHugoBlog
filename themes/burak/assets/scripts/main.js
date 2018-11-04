2f36a958c9b901dc81aebf8b1473af1dddf97ca4
Patrick Zielinski <lmno.patrick@gmail.com>
2018-11-03 20:30:45 -0400

    Add email validation to sub

--- themes/burak/assets/scripts/main.js
+++ themes/burak/assets/scripts/main.js
@@ -2,9 +2,27 @@ var app = new Vue({
   el: '#sub',
   data: {
     subBtnLabel: 'Subscribe',
-    isBtnDisabled: false,
+    email: '',
+  },
+  computed: {
+    cannotSubmit() {
+        return ! this.isValidEmail;
+    },
+    isValidEmail() {
+        return this.validEmail(this.email);
+    },
+    classesOnValidation() {
+        return [this.isValidEmail ? 'fas fa-check is-valid' : 'fas fa-exclamation is-invalid']; 
+    }
   },
   methods: {
+    validEmail: function (email) {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+            return true;
+        }
+        return false;
+    },
+
     subClicked: function () {
       this.subBtnLabel = 'Subscribed!'
       this.isBtnDisabled = true;
